669285dd9a270e8fac8478f4903e8422ea91f968
Chesa <brmmalesela@gmail.com>
2021-07-20 13:59:52 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyComponent } from './property/property.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      NavBarComponent,
+      PropertyComponent,
+      PropertyCardComponent,
+      PropertyListComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
